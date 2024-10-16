c4c17f7a7cc68b9f7f8729315b9b5e900b348d72
RahulGupta <rahulrgd21@gmail.com>
2024-10-16 21:44:40 +0530

    Initial commit - added all basic dependencies

---
+++ src/main/java/com/d5/ledgerflux/LedgerfluxApplication.java
@@ -0,0 +1,13 @@
+package com.d5.ledgerflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerfluxApplication.class, args);
+	}
+
+}
